a8c6b0e08e7ffd6829e0a713bfee427cc79c1155
Lucas Lacava <lucas.lacava@gmail.com>
2021-01-16 20:27:32 -0300

    first commit

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import './style.scss'
+import Lottie from 'react-lottie'
+import animationData from '../../lotties/kombosa.json'
 
-const HomeTemplate: React.FC = () => {
+const HomeTemplate = () => {
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice'
+		}
+	}
+
 	return (
-		<div className='flex flex-col justify-center items-center select-none min-h-screen bg-gradient-to-br from-gray-900  to-blue-700'>
-			<h1 className='text-6xl text-green-500 border-b-4 pb-4'>Hello World !</h1>
-			<h2 className='text-2xl text-gray-300 mt-10'>
-				Click{' '}
-				<a
-					className='text-green-400 underline'
-					target='_blank'
-					rel='noreferrer'
-					href='https://github.com/axibord/react-typescript-starter'
-				>
-					here
-				</a>{' '}
-				to read instructions for production optimization
-			</h2>
+		<div className='flex flex-col justify-center items-center select-none min-h-screen bg-gradient-to-br from-gray-500  to-indigo-700'>
+			<h1 className='text-6xl text-indigo-50  pb-4'>bora!</h1>
+			<Lottie isClickToPauseDisabled={true} options={defaultOptions} height={400} width={400} />
 		</div>
 	)
 }
